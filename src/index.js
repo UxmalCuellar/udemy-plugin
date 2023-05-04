efb12c553a30ff13dcbc4237ad492da3a74ecd32
Uxmal <uxmal.c@gmail.com>
2023-05-04 14:24:24 +1000

    block working in gutenberg

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log('test message');
+import { registerBlockType } from '@wordpress/blocks'
+import { RichText, useBlockProps } from '@wordpress/block-editor'
+import { __ } from '@wordpress/i18n'
+import block from './block.json'
+
+registerBlockType(block.name, {
+    edit({ attributes, setAttributes }) {
+        const { content } = attributes
+        const blockProps = useBlockProps()
+
+        return (
+            <RichText
+                {...blockProps}
+                tagName="h2"
+                placeholder={__('Enter Heading', 'udemy-plus')}
+                value={content}
+                onChange={newVal => setAttributes({ content: newVal })}
+            />
+        )
+    },
+    save({ attributes }) {
+        const { content } = attributes
+        const blockProps = useBlockProps.save()
+        return (
+            <RichText.Content
+                {...blockProps}
+                tagName="h2"
+                value={content}
+            />
+        )
+    
+    }
+})
